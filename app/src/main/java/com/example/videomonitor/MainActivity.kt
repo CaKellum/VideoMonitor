23d1fb09423652cd42ff8b9ee115ead8200488a9
= <=Christopherk379@gmail.com>
2022-06-28 21:21:07 -0400

    Update Apis and Add Navigation Code

--- app/src/main/java/com/example/videomonitor/MainActivity.kt
+++ app/src/main/java/com/example/videomonitor/MainActivity.kt
@@ -6,28 +6,52 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.example.videomonitor.ui.theme.VideoMonitorTheme
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            VideoMonitorTheme {
-                Column(modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally) {
-                    Button(onClick = { /*TODO*/ }) {
-                        Text(text = "Start Stream")
-                    }
-                    Button(onClick = { /*TODO*/ }) {
-                        Text(text = "Watch Stream")
-                    }
-                    Button(onClick = { /*TODO*/ }) {
-                        Text(text = "Settings")
-                    }
-                }
+            val navHost = NavHostController(context = this)
+            NavHost(navController = navHost,
+                startDestination = "home") {
+                composable("home") { HomeScreen(navController = navHost)}
+                composable("go_live") { Settings(navController = navHost)}
+                composable("watch_stream") { Settings(navController = navHost) }
+                composable("settings") { Settings(navController = navHost) }
             }
+        }
+    }
+}
+
+@Composable
+fun Settings(navController: NavController) {}
+
+fun GoLive(navController: NavController) {}
+
+fun WatchStream(navController: NavController) {}
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { navController.navigate("go_live") }) {
+            Text(text = "Go Live")
+        }
+        Button(onClick = { navController.navigate("watch_stream")}) {
+            Text(text = "Watch Stream")
+        }
+        Button(onClick = {navController.navigate("settings")}) {
+            Text(text = "Settings")
         }
     }
 }
